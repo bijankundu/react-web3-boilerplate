9907944c243178fc20ade47666f0d0763b97aa7a
Bijan Kundu <bijankundu025@gmail.com>
2022-07-23 11:31:18 +0530

    added builds

---
+++ build/static/js/56.f2e6a32e.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmiko_react=self.webpackChunkmiko_react||[]).push([[56],{32772:function(){},88924:function(){}}]);
---
+++ build/static/js/707.c30eece8.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmiko_react=self.webpackChunkmiko_react||[]).push([[707],{11922:function(){},22399:function(){},78848:function(){},71456:function(){}}]);
